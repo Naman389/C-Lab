588eafeacbfabc163fcf75c0a5e8771605cff322
Naman389 <namansingh6386@gmail.com>
2025-11-10 10:19:00 +0530

    Add files via upload

---
+++ filehandling5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    char text[100];
+    FILE *fp;
+    fp=fopen("test1.txt","a+");
+    if(fp==NULL){
+        printf("File is empty");
+        return 0;
+    }else{
+        fprintf(fp,"My name is Naman!");
+        //fgets(text,50,fp);
+        //printf("%s",text);
+        // fclose(fp);
+        //fp-=100;
+     
+        
+    }
+     //fp=fopen("test1.txt","");
+     fgets(text,-100,fp);
+    printf("%s",text);
+    fclose(fp);
+    return 0;
+}
